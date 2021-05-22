6f6352967d71f02507c4e08c3fd7ddb6b4d9827f
Ali Akbar <63883870+Cheval5@users.noreply.github.com>
2021-05-22 12:20:54 -0400

    host changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/images');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-cylindrical-08918',
         user: 'aliakbarchevalwala',
         password: 'rootroot',
         database: 'detect-me'
