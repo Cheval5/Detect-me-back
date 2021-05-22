da910b06b08975054bafd0d4437887787615bbc1
Ali Akbar <63883870+Cheval5@users.noreply.github.com>
2021-05-21 22:35:04 -0400

    looking into security

--- controllers/images.js
+++ controllers/images.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'c6c352d3c9bd4aeb917de8661fc9c894',
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(
+    Clarifai.FACE_DETECT_MODEL,
+    req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json("unable to work with api"))
+}
+
 const handleImages = (db) => (req, res) => {
     const { id } = req.body;
     db('users')
@@ -10,5 +25,6 @@ const handleImages = (db) => (req, res) => {
 }
 
 module.exports = {
-    handleImages: handleImages
+    handleImages,
+    handleApiCall
 }
